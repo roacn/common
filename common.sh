386ce8711509db7236f856b88e2c781e26643f49
roacn <roa@qq.com>
2023-10-14 19:07:55 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -232,14 +232,12 @@ function git_clone_source() {
 function update_packages() {
 	gitdate=$(curl -H "Authorization: token ${REPO_TOKEN}" -s "https://api.github.com/repos/${PACKAGES_ADDR}/actions/runs" | jq -r '.workflow_runs[0].created_at')
 	gitdate=$(date -d "$gitdate" +%s)
-	__info_msg "gitdate=${gitdate}"
 	now=$(date -d "$(date '+%Y-%m-%d %H:%M:%S')" +%s)
-	__info_msg "now=${now}"
 	if [[ $(expr $gitdate + 60) < $now ]]; then
 	curl -X POST https://api.github.com/repos/${PACKAGES_ADDR}/dispatches \
 	-H "Accept: application/vnd.github.everest-preview+json" \
 	-H "Authorization: token ${REPO_TOKEN}" \
-	--data '{"event_type": "updated by ${REPOSITORY}"}'
+	--data "{\"event_type\": \"updated by ${REPOSITORY}\"}"
 	fi
 	__info_msg "packages url: https://github.com/${PACKAGES_ADDR}"
 }
